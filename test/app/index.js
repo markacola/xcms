1a366c4dc10e42313b28cd0e2830fb30ffe41bab
Mark Cola <mark.cola1@gmail.com>
2016-07-19 22:03:19 +1000

    added tests

--- test/app/index.js
+++ test/app/index.js
@@ -1,4 +1,4 @@
-
+import sinon from 'sinon'
 import assert from 'assert'
 
 import App, {Plugin, Packet} from '../../src/app'
@@ -14,48 +14,71 @@ describe('App', () => {
     unhandledPacket = new Packet({type: 'unhandled-packet'})
   })
 
+  describe('constructor', () => {
+
+    it('should apply opts to app', () => {
+      let app = new App({name: 'my-test-app', someOption: true})
+      app.name.should.equal('my-test-app')
+      app.someOption.should.be.true()
+    })
+
+  })
+
   describe('use', () => {
+
     it('should register new plugin', () => {
       app.use(plugin)
       app.plugins.has(plugin).should.be.true()
     })
+
     it('should throw a type error', () => {
       (() => {
         app.use('not a plugin')
       }).should.throw(TypeError)
     })
+
   })
 
   describe('unuse', () => {
+
     it('should deregister plugin', () => {
       app.use(plugin)
       app.unuse(plugin)
       app.plugins.has(plugin).should.be.false()
     })
+
     it('should throw a type error', () => {
       (() => {
         app.unuse('not a plugin')
       }).should.throw(TypeError)
     })
+
   })
 
   describe('handlePacket', () => {
+
     it('should handle packet', () => {
+      let spy = sinon.spy(app, 'handlePacket')
       app.use(plugin)
       plugin.emit('packet', packet)
+
+      spy.calledOnce.should.be.true()
     })
+
     it('should throw type error', () => {
       (() => {
         app.use(plugin)
         plugin.emit('packet', 'not a packet')
       }).should.throw(TypeError)
     })
+
     it('should throw UnhandledPacketError', () => {
       (() => {
         app.use(plugin)
         plugin.emit('packet', unhandledPacket)
       }).should.throw(UnhandledPacketError)
     })
+
   })
 
 })
--- test/plugin/index.js
+++ test/plugin/index.js
@@ -8,6 +8,8 @@ describe('Plugin', () => {
   let app, pluginA, pluginB, pluginC, packetA, packetB
 
   beforeEach(() => {
+    packetA = new Packet({type: 'test-packet-a'})
+    packetB = new Packet({type: 'test-packet-b'})
     class PacketProducerPlugin extends Plugin {
       onMount() {
         this.emit(packetA)
@@ -20,24 +22,154 @@ describe('Plugin', () => {
     pluginA = new Plugin({name: 'test-a', handles: 'test-packet-a'})
     pluginB = new Plugin({name: 'test-b', handles: 'test-packet-b'})
     pluginC = new PacketProducerPlugin({name: 'test-c'})
-    packetA = new Packet({type: 'test-packet-a'})
-    packetB = new Packet({type: 'test-packet-b'})
   })
 
   describe('onMount', () => {
-    it('should register new plugin', function() {
-      this.timeout(50)
-      let spy = sinon.spy(app, 'handlePacket')
 
-      app.use(pluginA, pluginB, pluginC)
-      app.unuse(pluginC)
+    it('should call mount', () => {
+      let spy = sinon.spy(pluginA, 'onMount')
+
+      app.use(pluginA)
+
+      spy.calledOnce.should.be.true()
+      spy.getCall(0).args[0].should.equal(app)
+    })
+
+  })
+
+  describe('onUnmount', () => {
+
+    it('should call unmount', () => {
+      let spy = sinon.spy(pluginA, 'onUnmount')
+
+      app.use(pluginA)
+      app.unuse(pluginA)
+
+      spy.calledOnce.should.be.true()
+      spy.getCall(0).args[0].should.equal(app)
+    })
+
+  })
+
+  describe('consumePacket', () => {
+
+    it('should call consumePacket', () => {
+      let consumePacket = sinon.spy(pluginA, 'consumePacket')
+
+      app.use(pluginA)
+
+      pluginA.emit(packetA)
+
+      consumePacket.calledOnce.should.be.true()
+      consumePacket.getCall(0).args[0].should.equal(packetA)
+    })
+
+  })
+
+  describe('canHandlePacket', () => {
+
+    it('should test straight equity', () => {
+      let canHandlePacket = sinon.spy(pluginA, 'canHandlePacket')
+
+      app.use(pluginA)
+
+      pluginA.emit(packetA)
+
+      canHandlePacket.calledOnce.should.be.true()
+      canHandlePacket.getCall(0).args[0].should.equal(packetA)
+      canHandlePacket.returned(true)
+    })
+
+    it('should test packet type in set', () => {
+      let pluginA = new Plugin({name: 'test-a', handles: new Set(['test-packet-a', 'test-packet-b'])})
+      let canHandlePacket = sinon.spy(pluginA, 'canHandlePacket')
+
+      app.use(pluginA)
+
+      pluginA.emit(packetA)
+      pluginA.emit(packetB)
+
+      canHandlePacket.calledTwice.should.be.true()
+      canHandlePacket.getCall(0).args[0].should.equal(packetA)
+      canHandlePacket.getCall(1).args[0].should.equal(packetB)
+      canHandlePacket.returned(true)
+    })
+
+    it('should test packet type matches regex', () => {
+      let pluginA = new Plugin({name: 'test-a', handles: /test-packet-[ab]/})
+      let canHandlePacket = sinon.spy(pluginA, 'canHandlePacket')
+
+      app.use(pluginA)
+
+      pluginA.emit(packetA)
+      pluginA.emit(packetB)
+
+      canHandlePacket.calledTwice.should.be.true()
+      canHandlePacket.getCall(0).args[0].should.equal(packetA)
+      canHandlePacket.getCall(1).args[0].should.equal(packetB)
+      canHandlePacket.returned(true)
+    })
+
+  })
+
+  describe('dynamic packet generation', () => {
+
+    it('should generate sub-packets', (done) => {
+      class PacketA extends Packet {
+        constructor() {
+          super({type: 'packet-a'})
+        }
+      }
+      class PacketB extends Packet {
+        constructor() {
+          super({type: 'packet-b'})
+        }
+      }
+      class PacketC extends Packet {
+        constructor() {
+          super({type: 'packet-c'})
+        }
+      }
+      class PluginA extends Plugin {
+        constructor() {
+          super({name: 'plugin-a', handles: 'packet-a'})
+        }
+        consumePacket(packet) {
+          if (packet.type === 'packet-a') {
+            return this.consumePacketA(packet)
+          }
+        }
+        consumePacketA(packetA) {
+          this.emit(new PacketB())
+          setTimeout(() => {
+            this.emit(new PacketC())
+          }, 10)
+        }
+      }
+      class PluginB extends Plugin {
+        constructor() {
+          super({name: 'plugin-b', handles: /packet-[bc]/})
+        }
+      }
+
+      let pluginA = new PluginA()
+      let pluginB = new PluginB()
+
+      let spy = sinon.spy(pluginB, 'consumePacket')
+
+      app.use(pluginA, pluginB)
+
+      let packetA = new PacketA()
+      pluginA.emit(packetA)
 
       setTimeout(() => {
         spy.calledTwice.should.be.true()
-        spy.getCall(0).args[0].type.should.be('test-packet-a')
-        spy.getCall(1).args[0].type.should.be('test-packet-b')
-      }, 30)
+        spy.getCall(0).args[0].type.should.equal('packet-b')
+        spy.getCall(1).args[0].type.should.equal('packet-c')
+        done()
+      }, 15)
     })
+
   })
 
 })
